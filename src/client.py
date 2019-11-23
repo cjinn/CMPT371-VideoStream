3cb2d7fb50ed558e165844bc6d3b6dc47a0162c9
Choong Jin Ng <jin8383@gmail.com>
2019-11-22 23:43:44 -0800

    Encapsulate VideoStream object inside VideoStream.py

--- src/client.py
+++ src/client.py
@@ -1,22 +1,3 @@
-import cv2
-import numpy as np
-import socket
-import sys
-import pickle
-import struct
+import VideoStream as vs
 
-cap=cv2.VideoCapture(0)
-clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-clientsocket.connect(('localhost',8089))
-
-while True:
-    ret,frame=cap.read()
-    # Serialize frame
-    data = pickle.dumps(frame)
-
-    # Send message length first
-    message_size = struct.pack("L", len(data)) ### CHANGED
-
-    # Then data
-    clientsocket.sendall(message_size + data)
-    
+client = vs.Client()
